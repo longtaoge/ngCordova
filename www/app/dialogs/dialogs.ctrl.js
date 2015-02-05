1cc12b18d0875b7e8d898940afc62bb07733385f
longtaoge <61852263@qq.com>
2015-02-05 17:13:08 +0800

    添加对话框示例

--- www/app/dialogs/dialogs.ctrl.js
+++ www/app/dialogs/dialogs.ctrl.js
@@ -2,32 +2,28 @@ angular.module('demo.dialogs.ctrl', [])
 
   .controller('DialogsCtrl', function ($scope, $cordovaDialogs) {
 
-    $scope.action = "Press any button";
+    $scope.action = "按下任意按钮测试";
 
     $scope.alert = function () {
       $scope.action = "Alert";
-      $cordovaDialogs.alert('Wow!');
+      $cordovaDialogs.alert('Wow!旺！','标题','确认');
     };
 
     $scope.confirm = function () {
       $scope.action = "Confirm";
-      $cordovaDialogs.confirm('Are you sure?', "Custom title").then(function (buttonIndex) {
-        $cordovaDialogs.alert("Button index : " + buttonIndex);
+      $cordovaDialogs.confirm('确认提交吗？', "标题",["确认","取消"]).then(function (buttonIndex) {
+        $cordovaDialogs.alert("按钮索引 : " + buttonIndex);
       });
     };
-
     $scope.prompt = function () {
       $scope.action = "Prompt";
-      $cordovaDialogs.prompt('Please Login', "Custom title").then(function (result) {
-       $cordovaDialogs.alert("Input: " + result.input1 + "\n Button index : " + result.buttonIndex);
+      $cordovaDialogs.prompt('请登录', "标题",["确认","取消"]).then(function (result) {
+       $cordovaDialogs.alert("输入的值: " + result.input1 + "\n 按钮索引 : " + result.buttonIndex);
       });
     };
-
     $scope.beep = function () {
-      $scope.action = "Beep";
+      $scope.action = "Beep 峰鸣";
       $cordovaDialogs.beep(3);
     };
-
-
   });
 
