1d033a181ad371b77a98d0606cb4c9410a5ecd49
longtaoge <61852263@qq.com>
2015-02-05 10:58:27 +0800

    添加AppVersion

--- www/app/appRate/appRate.ctrl.js
+++ www/app/appRate/appRate.ctrl.js
@@ -2,11 +2,11 @@ angular.module('demo.appRate.ctrl', [])
 
   .controller('AppRateCtrl', function ($scope, $log, $cordovaAppRate) {
 
-      $scope.promptRating = function () {
-          $cordovaAppRate.promptForRating(true).then(function (result) {
-              console.log("result: " + result);
+      $scope.promptRating = function () {//定义一个方法，以响应页面中的点击事件x
+          $cordovaAppRate.promptForRating(true).then(function (result) {//参数为true表示显对对话框
+              console.log("result: " + result);//成功时的回调
           }, function (error) {
-              console.log("ERROR: " + error);
+              console.log("ERROR: " + error); //失败时的回调
           })
       };
   });
