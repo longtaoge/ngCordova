b0e19c3df1df327dd6be70a0184a9bbb1add0ba3
longtaoge <61852263@qq.com>
2015-02-14 12:50:10 +0800

    添加Statusbar示例

--- www/app/statusbar/statusbar.ctrl.js
+++ www/app/statusbar/statusbar.ctrl.js
@@ -1,12 +1,20 @@
 angular.module('demo.statusbar.ctrl', [])
-
-
   .controller('StatusbarCtrl', function ($scope, $cordovaStatusbar) {
-    $scope.toggleBar = function () {
+        $cordovaStatusbar.overlaysWebView(true);
+    $scope.toggleBar = function () { //切换状态栏状态
       if ($cordovaStatusbar.isVisible()) {
-        $cordovaStatusbar.hide();
+        $cordovaStatusbar.hide();//如果显示则隐藏
       } else {
-        $cordovaStatusbar.show();
+        $cordovaStatusbar.show();//如果隐藏则显示
       }
     };
+  $scope.setStyle=function(){//Android 平台下未起作用
+      //$cordovaStatusbar.overlaysWebView(true);
+      // styles: Default : 0, LightContent: 1, BlackTranslucent: 2, BlackOpaque: 3
+      $cordovaStatusbar.style(0);
+      // supported names: black, darkGray, lightGray, white, gray, red, green,
+      // blue, cyan, yellow, magenta, orange, purple, brown
+      $cordovaStatusbar.styleColor('white');
+      $cordovaStatusbar.styleHex('#00FFFF');
+  }
   });
