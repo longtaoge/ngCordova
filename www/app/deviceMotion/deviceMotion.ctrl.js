d863f5ed692a28a87956fc6f5a6ea68d68062f86
longtaoge <61852263@qq.com>
2015-02-14 17:07:07 +0800

    添加陀螺仪示例

--- www/app/deviceMotion/deviceMotion.ctrl.js
+++ www/app/deviceMotion/deviceMotion.ctrl.js
@@ -1,38 +1,29 @@
 angular.module('demo.deviceMotion.ctrl', [])
 
   .controller('DeviceMotionCtrl', function ($scope, $cordovaDeviceMotion) {
-    var watchID;
-
-    $cordovaDeviceMotion
-      .getCurrentAcceleration()
-      .then(function (motion) {
-        $scope.motion = motion;
-        console.log(motion);
-      }, function (err) {
-        $scope.msg = err.message;
-        console.log(err);
-      });
-
-
+        $scope.getAcceleration=function(){
+            $cordovaDeviceMotion
+                .getCurrentAcceleration()//获取当前的方位值
+                .then(function (motion) {
+                    $scope.motion = motion;
+                }, function (err) {
+                    $scope.msg = err.message;
+                });
+        };
     $scope.watchAcceleration = function () {
-      var options = { frequency: 50 };  // Update every 3 seconds
-
-      $scope.this_watch = $cordovaDeviceMotion.watchAcceleration(options);
-
-      $scope.this_watch.promise.then(
-        function () {  /* unused */
-        },
-        function (err) {
-          $scope.msg = err.message;
-        },
-        function (motion) {
-          $scope.motion = motion;
-        });
+      var options = { frequency: 3000 };  // 监控频率 单位为毫秒值
+       $scope.watch = $cordovaDeviceMotion.watchAcceleration(options);
+        $scope.watch.then(
+            null,
+            function(error) {
+               // 错误回调
+            },
+            function(result) {
+                $scope.motion = result;
+            });
     };
-
     $scope.clearWatch = function () {
-      // use watchID from watchAccelaration()
-      $cordovaDeviceMotion.clearWatch($scope.this_watch.watchId);
+       $scope.watch.clearWatch(); //停止监控
     };
   });
 
