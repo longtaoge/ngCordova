fa8d742402a2dda1e8c0ac3507be5614d45f66a2
longtaoge <61852263@qq.com>
2015-02-14 12:55:31 +0800

    添加进度条示例说明

--- www/app/progress/progress.ctrl.js
+++ www/app/progress/progress.ctrl.js
@@ -1,68 +1,65 @@
 angular.module('demo.progress.ctrl', ['ngCordova'])
 
-    .controller('ProgressCtrl', function ($scope, $log, $cordovaProgress) {
+    .controller('ProgressCtrl', function ($scope, $cordovaProgress,$timeout) {
+        $scope.loadProgressIndicator = function (type) {
+            switch (type) {
+                case  'indeterminate-simple':
+                    // ng-cordova 中的调用方法
+                    $cordovaProgress.showSimple(true);
+                    // 官方demo的调用方法
+                    ProgressIndicator.showSimple(true);
+                    timeout(5000);
+                    break;
+                case  'indeterminate-simple-then-hide':
+                    ProgressIndicator.showSimple(true);
+                    ProgressIndicator.hide();
+                    break;
+                case  'indeterminate-label':
+                    ProgressIndicator.showSimpleWithLabel(false, 'custom Label');
+                    timeout(5000);
+                    break;
+                case  'indeterminate-label-detail':
+                    ProgressIndicator.showSimpleWithLabelDetail(false, 'custom Label', 'custom detail');
+                    timeout(5000);
+                    break;
+                case  'determinate-simple':
+                    ProgressIndicator.showDeterminate(false);
+                    break;
+                case  'determinate-simple-label':
+                    ProgressIndicator.showDeterminateWithLabel(false, 100000, 'custom label');
+                    break;
+                case  'determinate-annular':
+                    ProgressIndicator.showAnnular(false);
+                    break;
+                case  'determinate-annular-label':
+                    ProgressIndicator.showAnnularWithLabel(false, 50000, 'custom label');
+                    break;
+                case  'determinate-bar':
+                    ProgressIndicator.showBar(false, 100000);
+                    break;
+                case  'determinate-bar-label':
+                    ProgressIndicator.showBarWithLabel(false, 100000, 'custom label');
+                    break;
+                case 'success':
+                    ProgressIndicator.showSuccess(false, 'success');
+                    timeout(5000);
+                    break;
+                case 'text-top':
+                    ProgressIndicator.showText(false, 'text at the TOP', 'top');
+                    timeout(5000);
+                    break;
+                case 'text-bottom':
+                    ProgressIndicator.showText(false, 'text at the BOTTOM', 'bottom');
+                    timeout(5000);
+                    break;
+            }
 
-        $scope.showSimple = function () {
-             alert("test");
-        //   ProgressIndicator.showSimple(true);
+            function timeout(time) {
+                $timeout(function () {
+                    ProgressIndicator.hide();
+                }, time);
 
-            $cordovaProgress.showSimple(ture);
-            alert("test1");
-        };
-
-     /*   $scope.showSimpleWithLabel = function () {
-
-            $cordovaProgress.showSimpleWithLabel(true, "Loading");
-        };
-        $scope.showSimpleWithLabelDetail = function () {
-
-            $cordovaProgress.showSimpleWithLabelDetail(true, "Loading", "detail")
-        };
-        $scope.showDeterminate = function () {
-
-            $cordovaProgress.showDeterminate(false, 100000)
-        };
-        $scope.showDeterminateWithLabel = function () {
-
-            $cordovaProgress.showDeterminateWithLabel(true, 50000, "Loading")
-        };
-
-        $scope.showAnnular = function () {
-
-            $cordovaProgress.showAnnular(true, 50000)
-        };
-
-        $scope.showAnnularWithLabel = function () {
-
-            $cordovaProgress.showAnnularWithLabel(false, 100000, "Loading")
-        };
-
-        $scope.showBar = function () {
-
-            $cordovaProgress.showBar(true, 50000)
-        };
-
-        $scope.showBarWithLabel = function () {
-
-            $cordovaProgress.showBarWithLabel(false, 100000, "Loading")
-        };
-
-        $scope.showSuccess = function () {
-
-            $cordovaProgress.showSuccess(true, "Success!")
-        };
-        $scope.showText = function () {
-
-            $cordovaProgress.showText(false, 100000, "Loading")
-        };
-*/
-
-        $scope.unshowSimple = function () {
-            alert("hide");
-            $cordovaProgress.hide();
-
-        };
-
-
+            }
+        }
 
     });
