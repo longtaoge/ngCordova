e103ba86d4534e73a07beaae1f144899ab24cb91
longtaoge <61852263@qq.com>
2015-02-14 12:18:29 +0800

    添加Toast示例

--- www/app/toast/toast.ctrl.js
+++ www/app/toast/toast.ctrl.js
@@ -1,21 +1,18 @@
 angular.module('demo.toast.ctrl', [])
 
-
   .controller('ToastCtrl', function ($scope, $cordovaToast) {
-
-    $scope.toastMessage = 'enter a message';
-
+    $scope.toastMessage = '这是一条吐丝';
     $scope.center = function (message) {
       $cordovaToast.show(message, 'long', 'center')
+          //在中间长时间显示一条信息
         .then(function (success) {
           console.log("center msg displayed");
         }, function (error) {
           $scope.msg = error.message;
         });
     };
-
     $scope.top = function (message) {
-
+        //在顶部显示一条信息
       $cordovaToast
         .showShortTop(message)
         .then(function (success) {
@@ -24,9 +21,8 @@ angular.module('demo.toast.ctrl', [])
           $scope.msg = error.message;
         });
     };
-
-
     $scope.bottom = function (message) {
+        //在底部显示一条吐丝
       $cordovaToast
         .showLongBottom(message)
         .then(function (success) {
