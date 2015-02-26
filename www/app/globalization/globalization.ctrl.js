b2c6012edd92bf493fa334e4971b0d816500dab9
longtaoge <61852263@qq.com>
2015-02-26 09:39:36 +0800

    添加  cordovaGlobalization  注释

--- www/app/globalization/globalization.ctrl.js
+++ www/app/globalization/globalization.ctrl.js
@@ -2,44 +2,34 @@ angular.module('demo.globalization.ctrl', [])
 
   .controller('GlobalizationCtrl', function ($scope, $cordovaGlobalization) {
 
-
     function init() {
       $cordovaGlobalization.getPreferredLanguage().then(
-        function (result) {
+        function (result) {  //成功后的回调函数
           $scope.language = result.value;
-            alert("language"+result.value);
         },
-        function (error) {
+        function (error) { //失败后的回调函数
           $scope.language = err.message;
         });
-
       $cordovaGlobalization.getLocaleName().then(
-        function (result) {
+        function (result) {//成功后的回调函数
           $scope.locale = result.value;
-            alert("locale"+result.value);
         },
-        function (error) {
+        function (error) {//失败后的回调函数
           $scope.locale = err.message;
         });
-
       $cordovaGlobalization.getFirstDayOfWeek().then(
-        function (result) {
+        function (result) {//成功后的回调函数
           $scope.fdow = result.value;
-            alert("FirstDoyOfWeek"+result.value);
         },
-        function (error) {
+        function (error) {//失败后的回调函数
           $scope.fdow = err.message;
         });
     }
-
-    $scope.refresh = function () {
+    $scope.refresh = function () {//刷新
       init();
     };
 
-
     init();
-
-
     // Soon implemented:
     // dateToString
     // stringToDate
