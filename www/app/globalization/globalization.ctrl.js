940ea12054b2e749486be00f9c0e8e19048def9a
longtaoge <61852263@qq.com>
2015-02-26 10:43:25 +0800

    添加 cordovaPreferences 注释

--- www/app/globalization/globalization.ctrl.js
+++ www/app/globalization/globalization.ctrl.js
@@ -1,43 +1,44 @@
 angular.module('demo.globalization.ctrl', [])
 
-  .controller('GlobalizationCtrl', function ($scope, $cordovaGlobalization) {
+    .controller('GlobalizationCtrl', function ($scope, $cordovaGlobalization) {
 
-    function init() {
-      $cordovaGlobalization.getPreferredLanguage().then(
-        function (result) {  //成功后的回调函数
-          $scope.language = result.value;
-        },
-        function (error) { //失败后的回调函数
-          $scope.language = err.message;
-        });
-      $cordovaGlobalization.getLocaleName().then(
-        function (result) {//成功后的回调函数
-          $scope.locale = result.value;
-        },
-        function (error) {//失败后的回调函数
-          $scope.locale = err.message;
-        });
-      $cordovaGlobalization.getFirstDayOfWeek().then(
-        function (result) {//成功后的回调函数
-          $scope.fdow = result.value;
-        },
-        function (error) {//失败后的回调函数
-          $scope.fdow = err.message;
-        });
-    }
-    $scope.refresh = function () {//刷新
-      init();
-    };
+        function init() {
+            $cordovaGlobalization.getPreferredLanguage().then(
+                function (result) {  //成功后的回调函数
+                    $scope.language = result.value;
+                },
+                function (error) { //失败后的回调函数
+                    $scope.language = err.message;
+                });
+            $cordovaGlobalization.getLocaleName().then(
+                function (result) {//成功后的回调函数
+                    $scope.locale = result.value;
+                },
+                function (error) {//失败后的回调函数
+                    $scope.locale = err.message;
+                });
+            $cordovaGlobalization.getFirstDayOfWeek().then(
+                function (result) {//成功后的回调函数
+                    $scope.fdow = result.value;
+                },
+                function (error) {//失败后的回调函数
+                    $scope.fdow = err.message;
+                });
+        }
 
-    init();
-    // Soon implemented:
-    // dateToString
-    // stringToDate
-    // getDatePattern
-    // getDateNames
-    // isDayLightSavingsTime
-    // numberToString
-    // stringToNumber
-    // getNumberPattern
-    // getCurrencyPattern
-  });
+        $scope.refresh = function () {//刷新
+            init();
+        };
+
+        init();
+        // Soon implemented:
+        // dateToString
+        // stringToDate
+        // getDatePattern
+        // getDateNames
+        // isDayLightSavingsTime
+        // numberToString
+        // stringToNumber
+        // getNumberPattern
+        // getCurrencyPattern
+    });
