57dc8580a7b641fad9d675b61de33e05e4a7b5b3
longtaoge <61852263@qq.com>
2015-02-26 15:03:29 +0800

    添加 flashlight  注释

--- www/app/flashlight/flashlight.ctrl.js
+++ www/app/flashlight/flashlight.ctrl.js
@@ -1,11 +1,13 @@
 angular.module('demo.flashlight.ctrl', [])
 
   .controller('FlashlightCtrl', function ($scope, $cordovaFlashlight) {
-    $scope.on = function () {
+    $scope.on = function () { //打开手电筒
       $cordovaFlashlight.switchOn();
     };
-    $scope.off = function () {
+    $scope.off = function () {//关闭手电筒
       $cordovaFlashlight.switchOff();
     };
-
+   $scope.toggle=function(){//切换手电筒状态
+       $cordovaFlashlight.toggle();
+   };
   });
