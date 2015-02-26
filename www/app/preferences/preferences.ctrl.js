a3b096dd11d5d4d3ee2ee750820dc3565ecf271d
longtaoge <61852263@qq.com>
2015-02-26 11:45:13 +0800

    添加 cordovaPreferences 注释

--- www/app/preferences/preferences.ctrl.js
+++ www/app/preferences/preferences.ctrl.js
@@ -1,15 +1,14 @@
 angular.module('demo.preferences.ctrl', [])
 
     .controller('PreferencesCtrl', function ($scope, $log, $cordovaPreferences) {
-
         var key = 'exampleKey';
         $scope.data = {};
         $scope.data.showMore = false;
         $scope.data.key = key;
         $scope.data.value = "";
         $scope.preferencesSet = function () {
-            alert("value="+$scope.data.value);
-            $cordovaPreferences.set(key, $scope.data.value)
+            alert("value=" + $scope.data.value);
+            $cordovaPreferences.set(key, $scope.data.value)//存储信息
                 .then(function (result) {
                     if (result) {
                         $log.log(key + ' was succesfully set to:', $scope.data.value);
@@ -21,8 +20,7 @@ angular.module('demo.preferences.ctrl', [])
                     $log.log(key + ' was not set to: ' + $scope.data.value + ' due to', err);
                 });
         };
-
-        $scope.preferencesGet = function () {
+        $scope.preferencesGet = function () {//获取指定信息
             $cordovaPreferences.get(key)
                 .then(function (value) {
                     $log.log(key + ' get was succesfully:', value);
@@ -31,6 +29,4 @@ angular.module('demo.preferences.ctrl', [])
                     $log.log(key + ' get was not succesfully: ' + $scope.data.value + ' due to', err);
                 });
         };
-
-
     });
--- www/app/printer/printer.ctrl.js
+++ www/app/printer/printer.ctrl.js
@@ -1,5 +1,15 @@
 angular.module('demo.printer.ctrl', [])
 
-  .controller('PrinterCtrl', function ($scope, $log, $cordovaPreferences) {
+    .controller('PrinterCtrl', function ($scope, $log, $cordovaPrinter) {/*$cordovaPreferences*/
+        var printerAvail = $cordovaPrinter.isAvailable();
+        alert("printerAvail=" + printerAvail.name);
+        var doc = "<html> ...PrenterTest </html>";
+        $scope.printerTest = function () {
+            try {
+                $cordovaPrinter.print(doc);//向打印机发送打印信息
+            }catch(err){
+            } finally{
 
-  });
+            }
+        };
+    });
