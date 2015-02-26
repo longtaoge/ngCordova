b74e6136d1e33646ed56339d4ef75c70ea599959
longtaoge <61852263@qq.com>
2015-02-26 12:56:08 +0800

    添加 cordovaPreferences 注释

--- www/app/batteryStatus/batteryStatus.ctrl.js
+++ www/app/batteryStatus/batteryStatus.ctrl.js
@@ -5,28 +5,42 @@ angular.module('demo.batteryStatus.ctrl', [])
         console.log("battery status init");
 
         $rootScope.batteryLevel = 1;       // (0 - 100)
+        $scope.batteryLevel=$rootScope.batteryLevel;
         $rootScope.isPluggedIn = true;   // bool
         $scope.monitor = function () {
             alert("monitor");
                 $rootScope.$on('$cordovaBatteryStatus:status', function (result) {
-                    var batteryLevel = result.level;       // (0 - 100)
+                   $rootscope.batteryLevel = result.level;       // (0 - 100)
                     var isPluggedIn = result.isPlugged;   // bool
-                    alert("" + batteryLevel + isPluggedIn);
+                    alert("" + $rootscope.batteryLevel + isPluggedIn);
                 });
 
                 $rootScope.$on('$cordovaBatteryStatus:critical', function (result) {
-                    var batteryLevel = result.level;       // (0 - 100)
+                    $rootscope.batteryLevel = result.level;       // (0 - 100)
                     var isPluggedIn = result.isPlugged;   // bool
+                    alert("" + $rootscope.batteryLevel + isPluggedIn);
                 });
 
                 $rootScope.$on('$cordovaBatteryStatus:low', function (result) {
-                    var batteryLevel = result.level;       // (0 - 100)
+                    $rootscope.batteryLevel = result.level;       // (0 - 100)
                     var isPluggedIn = result.isPlugged;   // bool
+                    alert("" + $rootscope.batteryLevel + isPluggedIn);
                 });
         };
 
         $scope.watchs = function () {
-           document.addEventListener("deviceready", function () {
+
+         /*   window.addEventListener("batterystatus", onBatteryStatus, false);
+
+            function onBatteryStatus(info) {
+                // Handle the online event
+                $rootScope.batteryLevel = info.level;
+                alert("level="+info.level);
+                console.log("Level: " + info.level + " isPlugged: " + info.isPlugged);
+            }
+*/
+
+            window.addEventListener("deviceready", function () {
 
                 $rootScope.$on('$cordovaBatteryStatus:status', function (result) {
                     $rootScope.batteryLevel = result.level;       // (0 - 100)
