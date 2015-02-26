4bf9511093949ffbbec8fc6e7eac3300015d4c21
longtaoge <61852263@qq.com>
2015-02-26 13:58:31 +0800

    添加进度条示例说明

--- www/app/vibration/vibration.ctrl.js
+++ www/app/vibration/vibration.ctrl.js
@@ -1,9 +1,7 @@
 angular.module('demo.vibration.ctrl', [])
 
   .controller('VibrationCtrl', function($scope, $cordovaVibration) {
-
     $scope.duration = 100;
-
     $scope.vibrate = function(){
       console.log("vibrating");
       $cordovaVibration.vibrate($scope.duration);
