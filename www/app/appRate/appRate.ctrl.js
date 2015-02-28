23d5d653287b9f8557fa65e73d40f216d93f5477
longtaoge <61852263@qq.com>
2015-02-28 13:46:24 +0800

    整理代码

--- www/app/appRate/appRate.ctrl.js
+++ www/app/appRate/appRate.ctrl.js
@@ -1,12 +1,12 @@
 angular.module('demo.appRate.ctrl', [])
 
-  .controller('AppRateCtrl', function ($scope, $log, $cordovaAppRate) {
+    .controller('AppRateCtrl', function ($scope, $log, $cordovaAppRate) {
 
-      $scope.promptRating = function () {//定义一个方法，以响应页面中的点击事件x
-          $cordovaAppRate.promptForRating(true).then(function (result) {//参数为true表示显对对话框
-              console.log("result: " + result);//成功时的回调
-          }, function (error) {
-              console.log("ERROR: " + error); //失败时的回调
-          })
-      };
-  });
+        $scope.promptRating = function () {//定义一个方法，以响应页面中的点击事件x
+            $cordovaAppRate.promptForRating(true).then(function (result) {//参数为true表示显对对话框
+                console.log("result: " + result);//成功时的回调
+            }, function (error) {
+                console.log("ERROR: " + error); //失败时的回调
+            })
+        };
+    });
